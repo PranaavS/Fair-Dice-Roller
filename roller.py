643d11172eea4528cc114ac7353ea56bbd91b6ff
PranaavS <126430053+PranaavS@users.noreply.github.com>
2023-07-13 22:23:16 -0500

    Update roller.py

--- roller.py
+++ roller.py
@@ -1,3 +1,4 @@
+import math
 import random
 import numpy as np
 import itertools
@@ -26,14 +27,58 @@ def generateProbabilites(numberOfDice=1, numberOfFaces=6):
         probabilites.update({i:getProbability(i, numberOfDice, numberOfFaces)})
     return probabilites
 
-probabilities = generateProbabilites(2,6)
+def generateGameRolls():
+    probabilities = generateProbabilites(2,6)
 
-possibilities = []
-correspondningProbabilities = []
-for key, value in probabilities.items():
-    if value != 0:
-        possibilities.append(key)
-        correspondningProbabilities.append(value)
+    possibilities = []
+    correspondningProbabilities = []
+    for key, value in probabilities.items():
+        if value != 0:
+            possibilities.append(key)
+            correspondningProbabilities.append(value)
 
-roll = random.choices(possibilities, correspondningProbabilities)
-print(roll[0])
+
+    rolls = []
+    for i in range(len(possibilities)):
+        for j in range(correspondningProbabilities[i]):
+            rolls.append(possibilities[i])
+
+    gameLength = 80
+    multiplyingFactor = math.ceil(gameLength/len(rolls))
+
+    gameRolls = []
+    for i in range(multiplyingFactor):
+        for j in rolls:
+            gameRolls.append(j)
+
+    with open("rolls.txt", "w") as f:
+        for i in gameRolls:
+            f.write(f"{i} \n")
+
+def Roll():
+    # Read the contents of the file into a list
+    with open("rolls.txt", "r") as f:
+        remainingRolls = []
+        for line in f.readlines():
+            remainingRolls.append(int(line.strip()))
+
+    # Choose a random item from the list
+    roll = random.choice(remainingRolls)
+
+    # Remove the chosen item from the list
+    remainingRolls.remove(roll)
+
+    # Write the updated list back to the file
+    with open("rolls.txt", "w") as f:
+        for item in remainingRolls:
+            f.write(str(item) + "\n")
+
+    return roll
+
+while __name__ == "__main__":
+    choice = input("Would you like to [r]oll or [c]reate a new game? ")
+    if choice == "c":
+        generateGameRolls()
+        print("A new game has been created!")
+    else:
+        print(Roll())
